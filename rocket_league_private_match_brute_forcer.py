d3e418161495ff40fec782c254c8f1dba63df811
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-04-30 15:27:15 +0200

    Added steam login functionality

--- rocket_league_private_match_brute_forcer.py
+++ rocket_league_private_match_brute_forcer.py
@@ -1,6 +1,4 @@
-import gevent.monkey
-gevent.monkey.patch_socket()
-gevent.monkey.patch_ssl()
+import json
 
 from steam import SteamClient
 
@@ -15,16 +13,25 @@ RLBuildId = '-1878310188'
 RLEnvironment = 'Prod'
 
 
+def steam_login(username, password):
+    steam_client = SteamClient()
+    two_factor_code = input('Enter 2FA> ')
+    steam_client.login(username, password, two_factor_code=two_factor_code)
+    return steam_client
+
+
 def auth():
     pass
 
 
 def main():
+    with open('steam_login.config') as steam_login_config_file:
+        steam_login_config = json.load(steam_login_config_file)
 
-    steam_client = SteamClient()
-    two_factor_code = input('Enter 2FA> ')
-    steam_client.login('chiliepeppers', 'suchsecure', two_factor_code=two_factor_code)
-    print(steam_client.friends)
+    steam_client = steam_login(steam_login_config['username'], steam_login_config['password'])
+    steam_id_64 = steam_client.user.steam_id.as_64
+    steam_app_ticket = steam_client.get_app_ticket(RLAppId).ticket
+    steam_display_name = steam_client.user.name
 
 
 if __name__ == '__main__':
