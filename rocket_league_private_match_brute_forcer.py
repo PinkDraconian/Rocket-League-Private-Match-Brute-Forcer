2d23719457c279a2c59b623a79f434524d6cca5e
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-05-01 18:01:06 +0200

    Successfully added authentication to Rocket League servers

--- rocket_league_private_match_brute_forcer.py
+++ rocket_league_private_match_brute_forcer.py
@@ -1,12 +1,16 @@
+import base64
+import hashlib
+import hmac
 import json
+import requests
 
-from steam import SteamClient
-
+# from steam import SteamClient
+from steam.client import SteamClient
 
 RLAppId = 252950  # https://steamdb.info/app/252950/
 RLEndpoint = 'https://psyonix-rl.appspot.com/Services'
 RLKey = 'c338bd36fb8c42b1a431d30add939fc7'
-RLUserAgent = 'RL Win/191113.75055.254904 gzip'
+RLUserAgent = 'RL Win/191113.75055.254903 gzip'
 RLLanguage = 'INT'
 RLFeatureSet = 'PrimeUpdate31'
 RLBuildId = '-1878310188'
@@ -20,18 +24,87 @@ def steam_login(username, password):
     return steam_client
 
 
-def auth():
-    pass
+def rl_auth(steam_id_64, steam_encoded_encrypted_app_ticket, steam_display_name):
+    data = json.dumps([{
+        'Service': 'Auth/AuthPlayer',
+        'Version': 1,
+        'ID': 1,
+        'Params': {
+            'Platform': 'Steam',
+            'PlayerName': steam_display_name,
+            'PlayerID': str(steam_id_64),
+            'Language': RLLanguage,
+            'AuthTicket': steam_encoded_encrypted_app_ticket,
+            'BuildRegion': '',
+            'FeatureSet': RLFeatureSet,
+            'bSkipAuth': False
+        }
+    }], separators=(',', ':'))
+
+    digest = hmac.new(RLKey.encode(), msg=('-' + data).encode(), digestmod=hashlib.sha256).digest()
+    signature = base64.b64encode(digest).decode()
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': RLUserAgent,
+        'Cache-Control': 'no-cache',
+        'PsyBuildID': RLBuildId,
+        'PsyEnvironment': RLEnvironment,
+        'PsyRequestID': 'PsyNetMessage_X_0',
+        'PsySig': signature
+    }
+
+    r = requests.post(url=RLEndpoint, headers=headers, data=data)
+    return json.loads(r.text)
+
+
+def encode_steam_encrypted_app_ticket(steam_encrypted_app_ticket):
+    result = [8, steam_encrypted_app_ticket.ticket_version_no, 16]
+
+    crc = steam_encrypted_app_ticket.crc_encryptedticket
+    while crc > 127:
+        result.append(crc & 127 | 128)
+        crc = (crc % 0x100000000) >> 7  # >>> in JavaScript
+    result.append(crc)
+
+    result.append(24)
+    result.append(steam_encrypted_app_ticket.cb_encrypteduserdata)
+
+    result.append(32)
+    result.append(steam_encrypted_app_ticket.cb_encrypted_appownershipticket)
+
+    result.append(42)
+    result.append(128)
+    result.append(1)
+    result.extend(steam_encrypted_app_ticket.encrypted_ticket)
+
+    return ''.join([hex(i)[2:].rjust(2, '0') for i in result]).upper()
 
 
 def main():
+    print('[STEAM] Parsing config file...')
     with open('steam_login.config') as steam_login_config_file:
         steam_login_config = json.load(steam_login_config_file)
 
+    print('[STEAM] Logging in...')
     steam_client = steam_login(steam_login_config['username'], steam_login_config['password'])
+    if steam_client.user is None:
+        print('[ERROR][STEAM] Login failed. Exiting.')
+        exit(1)
+    print('[STEAM] Successfully logged in!')
+
+    print('[STEAM] Getting id_64, encrypted_app_ticket and display_name...')
     steam_id_64 = steam_client.user.steam_id.as_64
-    steam_app_ticket = steam_client.get_app_ticket(RLAppId).ticket
+    steam_encrypted_app_ticket = steam_client.get_encrypted_app_ticket(252950, b'').encrypted_app_ticket
+    steam_encoded_encrypted_app_ticket = encode_steam_encrypted_app_ticket(steam_encrypted_app_ticket)
+
     steam_display_name = steam_client.user.name
+    print('[ROCKET LEAGUE] Authenticating with RL servers...')
+    rl_auth_response = rl_auth(steam_id_64, steam_encoded_encrypted_app_ticket, steam_display_name)
+    if rl_auth_response.get('Error', None) is not None:
+        print('[ERROR][ROCKET LEAGUE] Failed to authenticate with the RL servers. Exiting.')
+        exit(1)
+    print('[ROCKET LEAGUE] Successfully authenticated with the RL servers')
 
 
 if __name__ == '__main__':
