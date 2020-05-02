24e345141b4f7fcc0b0278ad29c2e5d70a73b268
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-05-02 09:51:39 +0200

    Refactored to conform with PEP8

--- rocket_league_private_match_brute_forcer.py
+++ rocket_league_private_match_brute_forcer.py
@@ -16,8 +16,10 @@ RLEndpoint = 'https://psyonix-rl.appspot.com/Services'
 RLKey = 'c338bd36fb8c42b1a431d30add939fc7'
 RLUserAgent = 'RL Win/191113.75055.254903 gzip'
 RLLanguage = 'INT'
-RLFeatureSet = 'PrimeUpdate31'  # Edit after Rocket League by launching the game with the `-log` option and looking for the new value
-RLBuildId = '-1878310188'  # Edit after Rocket League by launching the game with the `-log` option and looking for the new value
+# Edit after Rocket League by launching the game with the `-log` option and looking for the new value
+RLFeatureSet = 'PrimeUpdate31'
+# Edit after Rocket League by launching the game with the `-log` option and looking for the new value
+RLBuildId = '-1878310188'
 RLEnvironment = 'Prod'
 
 
