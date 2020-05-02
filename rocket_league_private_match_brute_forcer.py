fa13a191a8b9aabbe51f36ba47b2bd95c57c86d3
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-05-02 10:09:03 +0200

    Refactored rl_auth function into new rocket_league_authentication file

--- rocket_league_private_match_brute_forcer.py
+++ rocket_league_private_match_brute_forcer.py
@@ -3,49 +3,9 @@
 #   including RecursionError on Python 3.6. It may also silently lead to incorrect behaviour on Python 3.7.
 #   Please monkey-patch earlier. See https://github.com/gevent/gevent/issues/1016
 from authentication.steam_authentication import steam_login, encode_steam_encrypted_app_ticket
-
-import base64
-import hashlib
+from authentication.rocket_league_authentication import rl_auth
 import getpass
-import hmac
 import json
-import requests
-import rocket_league_constants
-
-
-def rl_auth(steam_id_64, steam_encoded_encrypted_app_ticket, steam_display_name):
-    data = json.dumps([{
-        'Service': 'Auth/AuthPlayer',
-        'Version': 1,
-        'ID': 1,
-        'Params': {
-            'Platform': 'Steam',
-            'PlayerName': steam_display_name,
-            'PlayerID': str(steam_id_64),
-            'Language': rocket_league_constants.RLLanguage,
-            'AuthTicket': steam_encoded_encrypted_app_ticket,
-            'BuildRegion': '',
-            'FeatureSet': rocket_league_constants.RLFeatureSet,
-            'bSkipAuth': False
-        }
-    }], separators=(',', ':'))
-
-    digest = hmac.new(rocket_league_constants.RLKey.encode(),
-                      msg=('-' + data).encode(), digestmod=hashlib.sha256).digest()
-    signature = base64.b64encode(digest).decode()
-
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': rocket_league_constants.RLUserAgent,
-        'Cache-Control': 'no-cache',
-        'PsyBuildID': rocket_league_constants.RLBuildId,
-        'PsyEnvironment': rocket_league_constants.RLEnvironment,
-        'PsyRequestID': 'PsyNetMessage_X_0',
-        'PsySig': signature
-    }
-
-    r = requests.post(url=rocket_league_constants.RLEndpoint, headers=headers, data=data)
-    return json.loads(r.text)
 
 
 def main():
