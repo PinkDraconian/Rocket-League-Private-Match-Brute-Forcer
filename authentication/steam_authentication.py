1d12d71ba751978d70f57c0c908ff079c6733a43
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-05-02 10:00:32 +0200

    Added docstring to encode_steam_encrypted_app_ticket and modified comments and docstring for steam_login

--- authentication/steam_authentication.py
+++ authentication/steam_authentication.py
@@ -5,7 +5,7 @@ from authentication.steam_login_exception import SteamLoginException
 
 
 def steam_login(username, password, two_factor_code):
-    """Logs in to steam
+    """Logs in to steam, returns logged in client if successful, raises SteamLoginException if not
     :param username: username of account
     :type username :class:`str`
     :param password: password of account
@@ -14,15 +14,22 @@ def steam_login(username, password, two_factor_code):
     :type two_factor_code :class:`str`
     :return steam_client of logged in user
     :rtype `SteamClient`
+    :raises `SteamLoginException`
     """
-    steam_client = SteamClient()
-    steam_client.login(username, password, two_factor_code=two_factor_code)
+    steam_client = SteamClient()  # Make steam client object
+    steam_client.login(username, password, two_factor_code=two_factor_code)  # Log in
     if not steam_client.logged_on:  # Login failed
         raise SteamLoginException('Login failed.')
     return steam_client
 
 
 def encode_steam_encrypted_app_ticket(steam_encrypted_app_ticket):
+    """Encodes encrypted app ticket.
+    :param steam_encrypted_app_ticket: encrypted app ticket
+    :type steam_encrypted_app_ticket :class:`EncryptedAppTicket`
+    :return: encoded app ticket
+    :rtype `str`
+    """
     result = [8, steam_encrypted_app_ticket.ticket_version_no, 16]
 
     crc = steam_encrypted_app_ticket.crc_encryptedticket
